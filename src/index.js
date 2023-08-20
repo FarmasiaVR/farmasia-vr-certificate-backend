18f3eaf9597d7bda34c031424dba79fe1330eddb
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-08-20 12:22:00 +0300

    install cors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 const http = require('http');
 const config = require('./config.js')
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+app.use(cors())
 
 
 app.get("/", (req, res) => {
   res.send("Hello Word")
 })
-
 
 app.listen(config.PORT, config.URL, () => {
   console.log(`Server running at http://${config.URL}:${config.PORT}/`);
