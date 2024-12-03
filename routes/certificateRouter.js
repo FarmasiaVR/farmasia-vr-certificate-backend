5fded8e01ac3ad502a3cf6544bba8a97722b514e
Juho <juho_tuomas@msn.com>
2024-12-03 18:32:16 +0200

    own router for certificate related code

---
+++ routes/certificateRouter.js
@@ -0,0 +1,44 @@
+const certificateRouter = require('express').Router()
+const mongoose = require('mongoose');
+const {Certificate} = require('../models/certificate.js');
+
+// Endpoint for receiving gameplay summaries
+certificateRouter.post("/create", async (req, res) => {
+    console.log(req.body)
+    const authorization = req.get('authorization')
+    const backendInfo = await Certificate.findOne()
+
+    if (authorization !== backendInfo.password) {
+      console.log("Unauthorized")
+      return res.send("Unauthorized")
+    }
+
+    console.log("Request is authorized")
+    // Implemement logic for sending data to Pate service TBA
+    // the email address is in backendInfo.email
+    
+    return res.status(201).end()
+})
+
+// Endpoint for changing either the pharmacy department receiving
+// email address or the password to be inserted in game.
+certificateRouter.put("/create_put", async (req, res) => {
+
+  // Create check for HY authentication 
+  console.log(req.body)
+  const updateCertificate = await Certificate.findOne()
+  updateCertificate.email = req.body.email
+  updateCertificate.password = req.body.password
+  
+  await updateCertificate.save()
+  return res.status(201).end()
+})
+
+certificateRouter.get("/", async (req, res) => {
+
+  // Create check for HY authentication 
+  const certificateInfo = await Certificate.findOne({})
+  return res.send(certificateInfo).end()
+})
+
+module.exports = certificateRouter
