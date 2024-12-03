963caa1905a17205468cf8e1c635b18cacc7a231
Juho <juho_tuomas@msn.com>
2024-12-03 18:34:00 +0200

    added routers and redis session management

--- index.js
+++ index.js
@@ -1,76 +1,36 @@
-const config = require('./config.js')
+const config = require('./utils/config.js')
 const express = require('express')
+const session = require('express-session')
 const cors = require('cors');
-const mongoose = require('mongoose');
+const passport = require('passport')
 
-mongoose.connect(config.MONGODB_URL).then(() => {
-  console.log("connected to database")
-})
-.catch((err) => {
-  console.log(err)
-})
+const loginRouter = require('./routes/loginRouter')
+const certificateRouter = require('./routes/certificateRouter')
 
-const {Certificate} = require('./models/certificate.js');
+//const setupAuthentication = require('./utils/oidc')
+const { setupDatabase } = require('./utils/db')
+const { redisConf } =require('./utils/redis')
+
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded());
+app.use(session(redisConf))
+//app.use(passport.initialize())
+//app.use(passport.session())
 
-// Creating the initial info in db with env values
-async function setupDatabase() {
-  const newCertificate = new Certificate({
-    email: config.DEFAULT_EMAIL,
-    password: config.DEFAULT_PASSWORD
-  })
-  await newCertificate.save()
-}
+app.use('/api/login', loginRouter)
+app.use('/api/certificates', certificateRouter)
 
-setupDatabase()
-
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
   res.send("Health check OK")
 })
 
-// Endpoint for receiving gameplay summaries
-app.post("/certificates/create", async (req, res) => {
-    console.log(req.body)
-    const authorization = req.get('authorization')
-    const backendInfo = await Certificate.findOne()
+// app.use(express.static("./dist"))
 
-    if (authorization !== backendInfo.password) {
-      console.log("Unauthorized")
-      return res.send("Unauthorized")
-    }
+app.listen(config.PORT, async () => {
+  // await setupAuthentication()
+  await setupDatabase()
 
-    console.log("Request is authorized")
-    // Implemement logic for sending data to Pate service TBA
-    // the email address is in backendInfo.email
-    
-    return res.status(201).end()
-})
-
-// Endpoint for changing either the pharmacy department receiving
-// email address or the password to be inserted in game.
-app.put("/certificates/create_put", async (req, res) => {
-
-  // Create check for HY authentication 
-  console.log(req.body)
-  const updateCertificate = await Certificate.findOne()
-  updateCertificate.email = req.body.email
-  updateCertificate.password = req.body.password
-  
-  await updateCertificate.save()
-  return res.status(201).end()
-})
-
-app.get("/certificates", async (req, res) => {
-
-  // Create check for HY authentication 
-  const certificateInfo = await Certificate.findOne({})
-  return res.send(certificateInfo).end()
-})
-
-app.listen(config.PORT, () => {
   console.log(`Server running at ${config.PORT}`);
 });
-
