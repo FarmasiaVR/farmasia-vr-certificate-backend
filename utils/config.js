547800bf7b4da7ce47b8f24efbe27ed69152680e
Juho <juho_tuomas@msn.com>
2024-12-05 16:14:44 +0200

    config to ESM

--- utils/config.js
+++ utils/config.js
@@ -1,28 +1,17 @@
-require('dotenv').config()
+import * as dotenv from 'dotenv'
 
-const PORT = process.env.PORT || 3001
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://root:example@mongo:27017/'
-const DEFAULT_EMAIL = process.env.DEFAULT_EMAIL
-const DEFAULT_PASSWORD = process.env.DEFAULT_PASSWORD
-const REDIS_HOST = process.env.REDIS_HOST || 'redis'
+dotenv.config()
 
-const OIDC_ISSUER = process.env.OIDC_ISSUER || ''
-const OIDC_CLIENT_ID = process.env.OIDC_CLIENT_ID || ''
-const OIDC_CLIENT_SECRET = process.env.OIDC_CLIENT_SECRET || ''
-const OIDC_REDIRECT_URI = process.env.OIDC_REDIRECT_URI || ''
-const SESSION_SECRET = process.env.SESSION_SECRET || ''
-const REDIS_URL = process.env.REDIS_URL || ''
+export const PORT = process.env.PORT || 3001
+export const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://root:example@mongo:27017/'
+export const DEFAULT_EMAIL = process.env.DEFAULT_EMAIL
+export const DEFAULT_PASSWORD = process.env.DEFAULT_PASSWORD
+export const REDIS_HOST = process.env.REDIS_HOST || 'redis'
 
-module.exports = {
-    PORT,
-    MONGODB_URL,
-    DEFAULT_EMAIL,
-    DEFAULT_PASSWORD,
-    REDIS_HOST,
-    OIDC_ISSUER,
-    OIDC_CLIENT_ID,
-    OIDC_CLIENT_SECRET,
-    OIDC_REDIRECT_URI,
-    SESSION_SECRET,
-    REDIS_URL
-}
+export const OIDC_ISSUER = process.env.OIDC_ISSUER || ''
+export const OIDC_CLIENT_ID = process.env.OIDC_CLIENT_ID || ''
+export const OIDC_CLIENT_SECRET = process.env.OIDC_CLIENT_SECRET || ''
+export const OIDC_REDIRECT_URI = process.env.OIDC_REDIRECT_URI || ''
+export const SESSION_SECRET = process.env.SESSION_SECRET || ''
+export const REDIS_URL = process.env.REDIS_URL || ''
+
