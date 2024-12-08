aad32d6f763647dd93aa0db62e281889bdff7877
Juho <juho_tuomas@msn.com>
2024-12-09 00:25:33 +0200

    session middleware to certificateRouter

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import loginRouter from './loginRouter.js'
 import certificateRouter from './certificateRouter.js'
+import middleware from '../utils/middleware.js'
+
 
 const router = express()
 
 router.use('/login', loginRouter)
+router.use(middleware.sessionChecker)
 router.use('/certificates', certificateRouter)
 
 export default router
