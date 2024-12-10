c442232e2c4c70b9cb0f3e2915c2743028b16fd6
Juho <juho_tuomas@msn.com>
2024-12-10 16:53:00 +0200

    removed middleware from certificaterouter

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,10 @@
 import express from 'express'
 import loginRouter from './loginRouter.js'
 import certificateRouter from './certificateRouter.js'
-import middleware from '../utils/middleware.js'
-
 
 const router = express()
 
 router.use('/login', loginRouter)
-router.use(middleware.sessionChecker)
 router.use('/certificates', certificateRouter)
 
 export default router
