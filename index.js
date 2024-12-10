24d2af4b065020f5c65f01543a3713761c762d9a
Juho <juho_tuomas@msn.com>
2024-12-10 16:53:16 +0200

    removed passport and oidc from index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import express from 'express'
-import passport from 'passport'
 import session from 'express-session'
 import cors from 'cors'
 import path from 'path'
 
 import { PORT } from './utils/config.js'
 
-import setupAuthentication from './utils/oidc.js'
 import setupDatabase from './utils/db.js'
 import redisConf from './utils/redis.js'
 import middleware from './utils/middleware.js'
@@ -19,8 +17,6 @@ app.use(express.json())
 app.use(express.urlencoded());
 app.use(middleware.requestLogger)
 app.use(session(redisConf))
-app.use(passport.initialize())
-app.use(passport.session())
 
 app.use('/api', (req, res, next) => router(req, res, next))
 app.use('/api', (_, res) => res.sendStatus(404))
@@ -38,7 +34,6 @@ app.get('*', (_, res) => res.sendFile(INDEX_PATH))
 
 app.listen(PORT, async () => {
   await setupDatabase()
-  await setupAuthentication()
 
   console.log(`Server running at ${PORT}`);
 });
