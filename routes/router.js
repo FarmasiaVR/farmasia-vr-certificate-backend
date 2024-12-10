ded669c7586f9feec0ec47b069b03badbd544e00
Juho Siitonen <104775829+JuhoSiitonen@users.noreply.github.com>
2024-12-10 17:08:38 +0000

    shibboleth middleware to routes

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import loginRouter from './loginRouter.js'
 import certificateRouter from './certificateRouter.js'
+import shibbolethMiddleware from '../utils/shibboleth.js'
+import middleware from '../utils/middleware.js'
 
 const router = express()
 
+router.use(shibbolethMiddleware)
+router.use(middleware.sessionChecker)
 router.use('/login', loginRouter)
 router.use('/certificates', certificateRouter)
 
