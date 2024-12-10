dee8a02ac83e33abda15bbb63fe2b9a7ad094309
Juho <juho_tuomas@msn.com>
2024-12-10 16:52:04 +0200

    removed oidc and related variables

--- utils/config.js
+++ utils/config.js
@@ -6,10 +6,7 @@ export const PORT = process.env.PORT || 3001
 export const MONGODB_URL = process.env.MONGODB_URL
 export const DEFAULT_EMAIL = process.env.DEFAULT_EMAIL
 export const DEFAULT_PASSWORD = process.env.DEFAULT_PASSWORD
-export const OIDC_ISSUER = process.env.OIDC_ISSUER || ''
-export const OIDC_CLIENT_ID = process.env.OIDC_CLIENT_ID || ''
-export const OIDC_CLIENT_SECRET = process.env.OIDC_CLIENT_SECRET || ''
-export const OIDC_REDIRECT_URI = process.env.OIDC_REDIRECT_URI || ''
+
 export const SESSION_SECRET = process.env.SESSION_SECRET || 'secret'
 export const REDIS_URL = process.env.REDIS_URL
 
