3166d4cc791ac08f444395f323a98a2b00e917c9
jobatabs <2370315+jobatabs@users.noreply.github.com>
2025-04-15 15:40:27 +0300

    Nitpick: Add quotes to manual request script URL

--- manual_request/POST.sh
+++ manual_request/POST.sh
@@ -4,4 +4,4 @@ curl -X POST \
 -H "Authorization: valokuva" \
 -H "Content-Type: application/json" \
 --data '{"user":"tester", "process":"progress"}'  \
-https://opetushallinto.cs.helsinki.fi/farmasiavr-backend/api/certificates/create
+"https://opetushallinto.cs.helsinki.fi/farmasiavr-backend/api/certificates/create"
