b6c9b1e78129b0f1249dea8c9a181029eb499fc6
jobatabs <2370315+jobatabs@users.noreply.github.com>
2025-04-15 16:42:07 +0300

    Fix

--- farmasia-ui/src/pages/version.jsx
+++ farmasia-ui/src/pages/version.jsx
@@ -1,31 +1,40 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 
-const Version = () => {
+const ShowCurrent = () => {
+    const [hidden, setHidden] = useState(false)
     const [version, setVersion] = useState('')
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const response  = await axios.get("/version")
-                const data = response.data;
-                setVersion(data)
-                console.log(data);
-            } catch (error) {
-                console.error(error);
-            }
-        })
-    }, []);
+    const showCredentials = async (e) => {
+        e.preventDefault()
+        if (hidden) {
+            setHidden(false)
+            this.value == "Hide current version"
+            return
+        }
+        try {
+            const response = await axios.get("/farmasiavr/version")
+            const version = response.data
+            setVersion(version)
+            console.log(version)
+            setHidden(true)
+            this.value == "Show current version"
+        } catch {
+            console.error("Error fetching current version")
+            return
+        }
+    }
+
     return (
         <>
-            {
+            {hidden &&
                 <div>
-                    <h4>Version: {version}</h4>
-                </div>
-            }
+                    <h4>Current version: {version}</h4>
+                </div>}
+            <button onClick={showCredentials}>Show current version</button>
         </>
     )
 }
 
-export default Version;
+export default ShowCurrent;
